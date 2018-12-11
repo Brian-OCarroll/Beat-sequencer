fbf5a5cd627dcc0ea7687d765352eba90594d236
Brian O'Carroll <bocarroll36@gmail.com>
2018-12-11 12:27:39 -0800

    added toggle active status for dynamic css

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Pad(props) {
     return (
+        //use conditional classname to update the style 
+        //depending on status of pad
         <div 
         className={"pad " + (props.state === 1 ? 'active' : '') + (props.pos === props.id ? ' playing' : '')}
         id = {"pad"+props.padNumber}
