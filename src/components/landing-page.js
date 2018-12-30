5a2822e9fdd062ff20cc72564600d36587483df6
Brian O'Carroll <bocarroll36@gmail.com>
2018-12-30 13:20:43 -0800

    added landing page with app info

---
+++ src/components/landing-page.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link, Redirect} from 'react-router-dom';
+import SeqContainer from './sequencer-container'
+// import LoginForm from './login-form';
+
+export function LandingPage(props) {
+    // If we are logged in redirect straight to the user's dashboard
+    if (props.loggedIn) {
+        return <Redirect to="/home" />;
+    }
+
+    return (
+        <div className="home">
+            <h1>Welcome HyperLoop</h1>
+            <p>HyperLoop is a 1 bar 16 step drum sequencer with over 100 drum samples to choose from </p>
+            <p>Tracks can be added and removed with the click of a button and if logged in can be saved for 
+                future use</p>
+            <p>Click the midi-sounds logo for customizable EQ settings</p>
+            {/* <SeqContainer /> */}
+            <Link to="/home">Home</Link>
+        </div>
+    );
+}
+
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(LandingPage);
