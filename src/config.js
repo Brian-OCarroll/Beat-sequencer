4224e4e2dd4c5f001cb9ef46400c119780aa2806
Brian-OCarroll <38870765+Brian-OCarroll@users.noreply.github.com>
2020-01-27 09:47:57 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 // export const API_BASE_URL = 'https://hyper-looper-server.herokuapp.com/api';
 
-export const API_BASE_URL =
-process.env.REACT_APP_API_BASE_URL || 'http://localhost:4050/api';
+export const API_BASE_URL = 'http://localhost:4050/api';
