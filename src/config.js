6a79872f4c20007728f05ca26108f785f0ac6321
Brian-OCarroll <bocarroll36@gmail.com>
2020-04-09 22:42:04 -0700

    changed database uri

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = 'https://hyper-looper-server.herokuapp.com/api';
 
-export const API_BASE_URL = 'https://localhost:4050/api';
+export const API_BASE_URL = 'https://beat-server.brianocarroll.dev';
